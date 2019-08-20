30bd325449e34cda31f44bff01c19d910d05defd
Christa Stephens <christastephens@quickenloans.com>
2019-08-20 10:21:25 -0400

    completed app

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating a new list of strings
+            var plants = new List<string>();
+            plants.Add("snake");
+            plants.Add("zz");
+            plants.Add("pothos");
+            plants.Add("money tree");
+
+            //Iterating through the list
+            //read as show one plant in a list of plants
+            foreach (var plant in plants)
+            {
+                Console.WriteLine(plant + " ");
+                //will output name of each plant individually
+            }
+        }
+    }
+}
